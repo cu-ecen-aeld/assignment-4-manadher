4d841fb79ccc870e704a563c4b75372c2edcde2d
manadher <manadher@gmail.com>
2024-09-14 00:37:31 -0700

    Adding aesd package configuration

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '84a71be716dc24dfcb0714b4787a659d243234db'
+AESD_ASSIGNMENTS_VERSION = '916f52b1d7268634f35dc93832896f0a5353e173'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
